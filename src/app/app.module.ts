a66590f7ddde27acd5ea28c2deb6e5ddc24e1b07
Renaud Racinet <renaud.racinet@estiam.com>
2019-12-08 15:58:44 +0100

    feat(app): install all dependencies
    ng-bootstrap
    ngrx/store
    ngrx/effects
    ngrx/store-devtools

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,36 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// NgRx
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { EffectsModule } from '@ngrx/effects';
+const NGRX_MODULES = [
+  StoreModule.forRoot({}),
+  StoreDevtoolsModule.instrument({
+    name: 'plunch',
+    maxAge: 15
+  }),
+  EffectsModule.forRoot([])
+];
+
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { LoginComponent } from './login/login.component';
+import { PlacesListComponent } from './places/components/places/places-list.component';
+const THIRD_PARTY_MODULES = [
+  NgbModule
+];
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ...NGRX_MODULES,
+    ...THIRD_PARTY_MODULES
   ],
   providers: [],
   bootstrap: [AppComponent]
