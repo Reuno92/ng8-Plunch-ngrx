61f283a67f50be2c0408e752ea89da161be8f654
Renaud Racinet <renaud.racinet@estiam.com>
2019-12-08 16:00:18 +0100

    feat(Places): add placesListComponent and placesModules

---
+++ src/app/places/places.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {PlacesListComponent} from './components/places/places-list.component';
+
+
+
+@NgModule({
+  declarations: [
+    PlacesListComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class PlacesModule { }
